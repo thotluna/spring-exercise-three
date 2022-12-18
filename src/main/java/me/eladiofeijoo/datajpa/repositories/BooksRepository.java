834410f18ea3d5281401517549dd872a175ad688
thotluna <eladi.feijoo@gmail.com>
2022-12-17 20:10:21 -0400

    add book entity and repository

--- src/main/java/me/eladiofeijoo/datajpa/repositories/BooksRepository.java
+++ src/main/java/me/eladiofeijoo/datajpa/repositories/BooksRepository.java
@@ -2,6 +2,8 @@ package me.eladiofeijoo.datajpa.repositories;
 
 import me.eladiofeijoo.datajpa.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BooksRepository extends JpaRepository<Book, Long> {
 }
