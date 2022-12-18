636278551ab76d3ae7ca9b5794623ee565175244
thotluna <eladi.feijoo@gmail.com>
2022-12-17 23:22:58 -0400

    Comment informative code in console

--- src/main/java/me/eladiofeijoo/datajpa/DataJpaApplication.java
+++ src/main/java/me/eladiofeijoo/datajpa/DataJpaApplication.java
@@ -19,29 +19,29 @@ public class DataJpaApplication {
 		ApplicationContext context = SpringApplication.run(DataJpaApplication.class, args);
 
 		CarsRepository repository = context.getBean(CarsRepository.class);
-		repository.deleteAll();
+//		repository.deleteAll();
 		Car toyota = new Car(null, "Toyota", "Prius",  2010);
 		Car chevrolet = new Car(null, "Chevrolet", "Corsa",  2006);
 		Car mitsubishi = new Car(null, "Mitsubishi", "L-300",  2007);
 		repository.save(toyota);
 		repository.save(chevrolet);
 		repository.save(mitsubishi);
-		List<Car> cars = repository.findAll();
-		for (Car car : cars){
-			System.out.println(car);
-		}
+//		List<Car> cars = repository.findAll();
+//		for (Car car : cars){
+//			System.out.println(car);
+//		}
 
 		BooksServices services = context.getBean(BooksServices.class);
 		Book refactoring = new Book(null, "Refactoring: Improving the Design of Existing Code", "Martin Fowler", 432,
 				32.39, LocalDate.of(2018,11, 28), true);
 		Book uml = new Book(null, "UML Distilled: A Brief Guide to the Standard Object Modeling Language", "Martin " +
 				"Fowler", 191,32.39, LocalDate.of(2018,8, 30), true);
-		services.deleteAll();
+//		services.deleteAll();
 		services.save(refactoring);
 		services.save(uml);
-		for (Book book : services.findAll()){
-			System.out.println(book);
-		}
+//		for (Book book : services.findAll()){
+//			System.out.println(book);
+//		}
 
 	}
 
